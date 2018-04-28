bb6091bff8d475a049c96e9760cd40b6f7a15bb3
Your Name <binojishara21@gmail.com>
2018-04-28 10:15:33 +0530

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WOW } from 'wowjs/dist/wow.min';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor() { }
+
+  ngOnInit() {
+  }
+  ngAfterViewInit(){
+
+    new WOW().init();
+  }
+
 }
